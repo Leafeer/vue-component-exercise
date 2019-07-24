4275b1a605dd59205ca5d451c33ec545eea074ca
Leafeer <leafeer@163.com>
2019-07-24 17:48:55 +0800

    lalala

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,70 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+   <div class="hello">
+     {{msg}}
+    <Props v-model="parentMsg" :todos="a" :name="x"
+    @show-balance="show_balance($event)">
+    
+    </Props>
+   <div v-if="show">
+     ningdeyue
+   </div>
+   <input v-model="parentMsg">
   </div>
+ 
 </template>
 
 <script>
+import Props from './props.vue'
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  
+  methods: {
+    show_balance:function(data){
+      this.show = true;
+      console.log('data:',data)
+    }
+  },
   props: {
     msg: String
+  },
+  components:{
+    Props
+  },
+  watch: {
+    parentMsg(newV,oldV){
+      console.log(newV,oldV);
+    }
+  },
+   data() {
+    return {
+      parentMsg:'test',
+      show:false,
+      x: "yely",
+      a: [
+        {
+          id: "0001",
+          name: "username",
+          type: "text",
+          title: "账号",
+          placeholder: "abc"
+        },
+        {
+          id: "0002",
+          name: "password",
+          type: "text",
+          title: "密码",
+          placeholder: "123"
+        }
+      ]
+    };
   }
-}
+  
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+ <style scoped>
 h3 {
   margin: 40px 0 0;
 }
